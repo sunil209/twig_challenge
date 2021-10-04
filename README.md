94f6f13509a30b333a0e4d9c930a60d9285fe977
Sunil Verma <sunilverma@Sunils-MacBook-Air.local>
2021-10-04 10:27:33 +0100

    Adding Read me file as well

---
+++ README.md
@@ -0,0 +1,20 @@
+# Getting Started with to create function to slip Array into N
+
+This project is create at https://github.com/sunil209/twig_challenge/tree/master
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### index.html file
+
+In the console will have output.
+
+### Pass the Array and N Manually
+
+Eg.
+
+Use Function divideArrayIntoChunk(Array, N)
+
+Where Pass array wich need to be divide
+N is inter number of chunk value to divide array
--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-function chunkArray(myArray, chunk_size){
+function divideArrayIntoChunk(myArray, chunk_size){
     var results = [];
-    
     while (myArray.length) {
-
-        console.log(myArray.splice(0, chunk_size));
-        console.log(myArray);
-        
+        //console.log(myArray.splice(0, chunk_size));
+        //console.log(myArray);
         results.push(myArray.splice(0, chunk_size));
     }
-    
     return results;
 }
 
-// Split in group of 3 items
-var result = chunkArray([1,2,3,4,5,6,7,8], 2);
-// Outputs : [ [1,2,3] , [4,5,6] ,[7,8] ]
+var result = divideArrayIntoChunk([1,2,3,4,5,6,7,8], 2);
 console.log(result);
